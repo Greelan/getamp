73718c51bde656a63a30fdd26389d9812d1e71f3
Greelan <53196309+Greelan@users.noreply.github.com>
2025-10-24 08:40:43 +1100

    Tidies

--- getamp.sh
+++ getamp.sh
@@ -766,7 +766,7 @@ EOF
 function installDocker {
 	echo ""
 	
-	if [ "$DOCKER_IS_INSTALLED" ]; then
+	if [[ "$DOCKER_IS_INSTALLED" ]]; then
 		echo "Docker is already installed."
 		echo "If you didn't install Docker from the official Docker repositories, then it may not operate correctly with AMP."
 		echo "Do you want to remove the existing Docker installation and install Docker from the official Docker repositories, if available for your system?"
@@ -799,8 +799,6 @@ function installDocker {
 				[[ -f /usr/share/keyrings/download.docker.com.gpg ]] && rm -f /usr/share/keyrings/download.docker.com.gpg >/dev/null 2>&1
 				[[ -f /etc/apt/sources.list.d/download.docker.com.list ]] && rm -f /etc/apt/sources.list.d/download.docker.com.list >/dev/null 2>&1
 				[[ -f /etc/apt/sources.list.d/docker.list ]] && rm -f /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
-				wget -qO /usr/share/keyrings/docker.asc https://download.docker.com/linux/$BASE_ID/gpg
-				chmod a+r /usr/share/keyrings/docker.asc
 				if { [[ "$BASE_ID" == "ubuntu" ]] && version_ge "$BASE_VERSION_ID" "22.04"; } || { [[ "$BASE_ID" == "debian" ]] && version_ge "$BASE_VERSION_ID" "12"; }; then
 					printf "Types: deb\nURIs: https://download.docker.com/linux/%s\nSuites: %s\nComponents: stable\nArchitectures: %s\nSigned-By: /usr/share/keyrings/docker.asc\n" "$BASE_ID" "$BASE_SUITE" "$(dpkg --print-architecture)" \
 					| tee /etc/apt/sources.list.d/docker.sources > /dev/null
@@ -808,8 +806,13 @@ function installDocker {
 					echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.asc] https://download.docker.com/linux/$BASE_ID $BASE_SUITE stable" \
 					| tee /etc/apt/sources.list.d/docker.list > /dev/null
 				fi
-				$PM_COMMAND update &>> "$LOG_FILE"
-				DOCKER_PACKAGES="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+				{
+					install -d -m 0755 /usr/share/keyrings
+					wget -qO /usr/share/keyrings/docker.asc https://download.docker.com/linux/$BASE_ID/gpg
+					chmod a+r /usr/share/keyrings/docker.asc
+					$PM_COMMAND update
+					DOCKER_PACKAGES="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+				} &>> "$LOG_FILE"
 			fi
             ;;
         rhel|fedora|centos)
@@ -819,12 +822,16 @@ function installDocker {
 				if [[ "$reInstallDocker" =~ ^[Yy]$ ]]; then
 					REMOVE_DOCKER_PACKAGES="docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine podman runc"
 				fi
-				if [ "$PM_COMMAND" == "yum" ]; then
-					$PM_COMMAND "${PM_INSTALL[@]}" yum-utils
-					yum-config-manager --add-repo https://download.docker.com/linux/$BASE_ID/docker-ce.repo
-				elif [ "$PM_COMMAND" == "dnf" ]; then
-					$PM_COMMAND "${PM_INSTALL[@]}" dnf-plugins-core
-					$PM_COMMAND config-manager --add-repo https://download.docker.com/linux/$BASE_ID/docker-ce.repo
+				if [[ "$PM_COMMAND" == "yum" ]]; then
+					{
+						$PM_COMMAND "${PM_INSTALL[@]}" yum-utils
+						yum-config-manager --add-repo https://download.docker.com/linux/$BASE_ID/docker-ce.repo
+					} &>> "$LOG_FILE"
+				elif [[ "$PM_COMMAND" == "dnf" ]]; then
+					{
+						$PM_COMMAND "${PM_INSTALL[@]}" dnf-plugins-core
+						$PM_COMMAND config-manager --add-repo https://download.docker.com/linux/$BASE_ID/docker-ce.repo
+					} &>> "$LOG_FILE"
 				fi
 				DOCKER_PACKAGES="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
 			fi
@@ -839,7 +846,7 @@ function installDocker {
 		return
 	else
 		{
-			if [[ "$reInstallDocker" =~ ^[Yy]$ ]]l then
+			if [[ "$reInstallDocker" =~ ^[Yy]$ ]] && [[ -n "$REMOVE_DOCKER_PACKAGES" ]]; then
 				docker ps -q | xargs -r docker stop
 				systemctl stop docker
 				for pkg in $REMOVE_DOCKER_PACKAGES; do $PM_COMMAND "${PM_UNINSTALL[@]}" $pkg; done
